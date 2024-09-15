537eb4b6948b7f6d1bf41ddd4bb40683a5d00145
Igor Amaral Silva <igoramarals96@gmail.com>
2024-09-15 18:40:52 -0300

    Project created

---
+++ backend/src/main/java/com/mypetManager/myPetManager/MyPetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mypetManager.myPetManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPetManagerApplication.class, args);
+	}
+
+}
