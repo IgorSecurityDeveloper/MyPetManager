665860b05ea53b763d02c7b750a4b91ce8ce29b2
Igor Amaral Silva <igoramarals96@gmail.com>
2025-02-16 11:18:09 -0300

    SCRUM-3 Task
    Improve general CSS style and create SCSS file to pre-compile styles.

--- frontend/application/src/app/Pages/Dashboard/Dashboard.js
+++ frontend/application/src/app/Pages/Dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 'use client'
 
+// import globals from '../../globals.scss'
+
 function Dashboard() {
   return(
     <>
--- frontend/application/src/app/Pages/Login/Login.js
+++ frontend/application/src/app/Pages/Login/Login.js
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import { useAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-import '../../globals.css';
+import '../../globals.scss';
 import './Login.css';
 
 function Login() {
